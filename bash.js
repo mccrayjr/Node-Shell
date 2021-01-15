c652a5e24339768c56a65a932d0f9c3bd740d058
John M <mccrayjrob@gmail.com>
2021-01-15 15:49:20 -0500

    Updated with solutions

--- bash.js
+++ bash.js
@@ -1,10 +1,25 @@
-process.stdout.write('prompt >');
+const prompt = (data) => {
+  console.log(data);
+  process.stdout.write('> ');
+};
 
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
+const main = (data) => {
+  const entry = data.toString().trim();
+  const [command, arg] = entry.split(' ');
+  switch (command) {
+    case 'pwd':
+      require('./pwd')(prompt);
+      break;
+    case 'ls':
+      require('./ls')(prompt);
+      break;
+    case 'cat':
+      require('./cat')(arg, prompt);
+      break;
+    case 'curl':
+      require('./curl')(arg, prompt);
+      break;
+    default:
+      prompt('not found');
   }
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-});
+};
---
+++ ls.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+module.exports = (done) => {
+  fs.readdir('./', 'utf8', (err, data) => {
+    if (err) {
+      done(err.stack);
+    } else {
+      done(data.join('\n'));
+    }
+  });
+};
+
+module.exports = {
+  listObj,
+};
